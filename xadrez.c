1210e3bee890831e17afe4a4261fa52f75ae1ccf
Anastasiia Urvantseva <ana.urvantseva@gmail.com>
2025-10-24 21:33:42 +0000

    Movimentando as Peças do xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main() {
+
+    int i;
+
+    //Mover a torre 5 casas para a direita
+    for ( i = 0; i < 5; i++)
+    {
+        printf("Direita\n"); //imprimindo direção do movimento
+    }
+
+    //Mover a rainha 8 casas para a esquerda
+    for ( i = 0; i < 8; i++)
+    {
+        printf("Esquerda\n"); //imprimindo direção do movimento
+    }
+
+    //Mover a bispo 5 casas na diagonal para a direita
+    for ( i = 0; i < 5; i++)
+    {
+        printf("Cima Direita\n"); //imprimindo direção do movimento
+    }
+    
+    return 0;
+}
