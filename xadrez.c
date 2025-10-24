341709f0760ad19b66b257fb4c3584343cdc4caf
Anastasiia Urvantseva <ana.urvantseva@gmail.com>
2025-10-24 23:13:34 +0000

    Movimentando o Cavalo

--- xadrez.c
+++ xadrez.c
@@ -3,6 +3,7 @@
 int main() {
 
     int i;
+    int movimentoCompleto = 1; //para controlar movimento L
 
     //Mover a torre 5 casas para a direita
     for ( i = 0; i < 5; i++)
@@ -20,6 +21,17 @@ int main() {
     for ( i = 0; i < 5; i++)
     {
         printf("Cima Direita\n"); //imprimindo direção do movimento
+    }
+    
+    //Movimentando o Cavalo
+    while (movimentoCompleto--)
+    {
+        for (int i = 0; i < 2; i++) //2 vezes para cima, 1 para esquerda
+        {
+            printf("Cima\n");
+        }
+        printf("Esquerda\n");
+        
     }
     
     return 0;
